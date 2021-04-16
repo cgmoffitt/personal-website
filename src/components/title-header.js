15a75e77f4d6a60552904214a1cace6e0ff0f2ff
cgmoffitt <chris.moffitt16@gmail.com>
2021-04-15 20:20:31 -0700

    add fade in and out animation to title

--- src/components/title-header.js
+++ src/components/title-header.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from 'react'
 
 export default function TitleHeader(){
-    const titles = ["Software Engineer", "Product Manager", "Chief of Staff"]
+    const titles = [
+        <h2 className="stix-reg yellow-theme text-4xl fade-in">: Software Engineer</h2>, 
+        <h2 className="stix-reg yellow-theme text-4xl fade-in">: Product Manager</h2>, 
+        <h2 className="stix-reg yellow-theme text-4xl fade-in">: Chief of Staff</h2>]
 
     const [curTitle, setCurTitle] = useState(0)
 
@@ -16,8 +19,12 @@ export default function TitleHeader(){
     return (
         <div className="absolute top-1/3 right-1/4">
             <h1 className="raanan-bold text-white text-8xl">Chris Moffitt</h1>
-            <h2 className="stix-reg yellow-theme text-4xl">: {titles[curTitle]}</h2>
-            <h2 className="stix-reg grey-theme text-3xl py-3">Who likes to turn creative ideas into reality</h2>
+            <div>
+                <h2 className="stix-reg yellow-theme text-4xl fade-in-out">: Software Engineer</h2>
+                <h2 className="stix-reg yellow-theme text-4xl fade-in-out">: Product Manager</h2> 
+                <h2 className="stix-reg yellow-theme text-4xl fade-in-out">: Chief of Staff</h2>
+            </div>
+            <h2 className="stix-reg grey-theme text-3xl my-12">Who likes to turn creative ideas into reality</h2>
         </div>
         
     )
